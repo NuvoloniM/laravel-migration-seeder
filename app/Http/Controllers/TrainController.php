42763a90b74ba41de097861770ef2abfd8f441d8
NuvoloniM <nuvoloni.m@gmail.com>
2022-05-26 16:37:14 +0200

    logica SELECT in traincontroller

--- app/Http/Controllers/TrainController.php
+++ app/Http/Controllers/TrainController.php
@@ -10,7 +10,11 @@ use App\Models\Train;
 class TrainController extends Controller
 {
     public function index(){
-        $trains = Train::all();
+        // $trains = Train::all();
+        // da tutto il database seleziono solo i record che mi interessano, e non tutti 
+        // voglio solo treni che partono oggi 
+        $today = date("Y.m.d");
+        $trains = Train::where('departures_date', '=', $today)->get();
 
         return view('trains.index', compact('trains'));
     }
