1e1ac7935bbffb407161754365c1612b7517ca64
NuvoloniM <nuvoloni.m@gmail.com>
2022-05-26 15:14:31 +0200

    migration

---
+++ database/migrations/2022_05_26_130350_create_trains_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTrainsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('trains', function (Blueprint $table) {
+            $table->id();
+            $table->string('company', 50);
+            $table->string('departures', 50);
+            $table->string('arrivals', 50);
+            $table->string('train_code', 30)->unique();
+            $table->tinyInteger('carriages');
+            $table->boolean('in_time')->nullable()->default(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('trains');
+    }
+}
